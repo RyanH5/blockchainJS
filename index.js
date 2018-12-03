4fd3cbdb49789b6cc9e02df7556af64a538ccae7
Ryan Harrington <Ryanaharrington5@gmail.com>
2018-12-03 09:56:33 -0700

    Blockchain proof of work

--- index.js
+++ index.js
@@ -7,15 +7,26 @@ class Block {
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
+
   calculateHash() {
     return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+  }
+
+  mineBlock(difficulty) {
+    while(this.hash.substring(0, difficulty) != Array(difficulty + 1).join('0')) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log('BLOCK MINED: ' + this.hash)
   }
 }
 
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
   }
 
   createGenesisBlock() {
@@ -28,8 +39,25 @@ class Blockchain {
 
   addBlock(newBlock) {
     newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.mineBlock(this.difficulty);    
     this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for(let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i -1];
+
+      if(currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if(currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
   }
 }
 
@@ -37,4 +65,26 @@ let savjeeCoin = new Blockchain();
 savjeeCoin.addBlock(new Block(1, '10/17/2017', { amount: 4}));
 savjeeCoin.addBlock(new Block(2, "12/07/2017", { amount: 4}))
 
-console.log(JSON.stringify(savjeeCoin, null, 4))
+
+// THIS SHOWS THE BLOCKCHAIN PRETTY
+// console.log(JSON.stringify(savjeeCoin, null, 4))
+
+// THIS SHOWS THE BLOCKCHAIN VALIDATED
+// console.log('Is block chain valid', savjeeCoin.isChainValid());
+
+// THIS ALTERS THE DATA IN  BLOCK
+// savjeeCoin.chain[1].data = { amount: 100 }
+
+// THIS CALCULATES A NEW HASH FOR BLOCK[1]
+// savjeeCoin.chain[1].hash = savjeeCoin.chain[1].calculateHash();
+
+// THIS SHOWS THE BLOCKCHAIN INVALID AFTER ALTERING BLOCK[1] OR GIVING BLOCK[1] NEW HASH
+// console.log('Is block chain valid', savjeeCoin.isChainValid());
+
+// +++++++++++++++++++++++++ PROOF OF WORK VIDEO+++++++++++++++++++++++++
+
+console.log('MINING BLOCK 1...');
+savjeeCoin.addBlock(new Block(1, "20/07/2017", { amount: 5 }))
+
+console.log('MINING BLOCK 2...');
+savjeeCoin.addBlock(new Block(1, "20/07/2017", { amount: 10 }))
